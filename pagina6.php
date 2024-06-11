9e132b298488a7a0a1eb7f83da5865e6fbff5126
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 14:26:22 -0300

    pagina6.php

--- pagina6.php
+++ pagina6.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meses</title>
+</head>
+<body>
+    <h1>Meses</h1>
+    <?php
+$meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+sort($meses); 
+foreach ($meses as $mes) {
+    echo $mes . "<br>";
+}
+?>
+</body>
+</html>
