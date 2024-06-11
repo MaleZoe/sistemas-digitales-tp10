5022a3e3b579133945f242ef7798d4bfeae1424d
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 13:38:31 -0300

    pagina3.php

---
+++ pagina3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Importe</title>
+</head>
+<body>
+    <h1>Importe</h1>
+    <?php
+    $importe = array(0.9, 1.8, 4.6, 7.2);
+    echo "el importe es: " . $importe[1]; 
+    ?>
+</body>
+</html>
