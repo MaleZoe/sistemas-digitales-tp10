00dc790e380d678207331284144a6bb91b0e0199
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 15:01:37 -0300

    pagina7.php

---
+++ pagina7.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meses</title>
+</head>
+<body>
+    <h1>Meses</h1>
+    <?php
+  $usuarios = array(
+    'usuario1' => 'clave1',
+    'usuario2' => 'clave2',
+    'usuario3' => 'clave3',
+    'usuario4' => 'clave4',
+    'usuario5' => 'clave5'
+);
+
+echo "Clave de usuario1: " . $usuarios['usuario1'] . "<br>";
+echo "Clave de usuario2: " . $usuarios['usuario2'] . "<br>";
+echo "Clave de usuario3: " . $usuarios['usuario3'] . "<br>";
+echo "Clave de usuario4: " . $usuarios['usuario4'] . "<br>";
+echo "Clave de usuario5: " . $usuarios['usuario5'] . "<br>";
+?>
+
+</body>
+</html>
