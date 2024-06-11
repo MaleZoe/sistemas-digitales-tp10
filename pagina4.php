4315af1bd77284206f39f4abc8df41d26338d214
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 13:47:03 -0300

    pagina4.php

--- pagina4.php
+++ pagina4.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>booleano</title>
+</head>
+<body>
+    <h1>Booleano</h1>
+    <?php
+    $confirmado = array(true, true, false, true, false, false);
+    echo "El elemento con índice 0 es: " . ($confirmado[0] ? "true" : "false");
+    ?>
+</body>
+</html>
