a8b2ebcf403a1b04af2b9a0e6f284ef79f961716
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 12:51:20 -0300

    Update pagina1.php

--- pagina1.php
+++ pagina1.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Días de la Semana</title>
+</head>
+<body>
+    <?php
+    $dias_semana = array("lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo");
+    echo "Primer día de la semana: " . $dias_semana[0] . "<br>";
+    echo "Último día de la semana: " . end($dias_semana);
+    ?>
+</body>
+</html>
