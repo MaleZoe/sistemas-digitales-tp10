6468325ed67b766b00c8333dde4c29fbb8f36aa7
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-11 13:36:22 -0300

    pagina2.php

---
+++ pagina2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coches</title>
+</head>
+<body>
+    <h1>Coches</h1>
+    <?php
+    $coches = array(0, 1, 2, 3, 4, 5, 6, 7);
+    echo "Los coches son: " . $coches[5]; 
+    ?>
+</body>
+</html>
